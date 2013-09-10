9f1d2cd8c58a18fc90aa1423252ac47844129cfa
John Li <jzli@xpaperclip.net>
2013-09-11 02:01:44 +1000

    add UI InputBox

--- src/LxToolsBase/UI.cs
+++ src/LxToolsBase/UI.cs
@@ -19,6 +19,51 @@ namespace LxTools
 
     public static class UI
     {
+        public static DialogResult InputBox(string title, string prompt, ref string value)
+        {
+            using (var form = new Form())
+            using (var label = new Label())
+            using (var textBox = new TextBox())
+            using (var buttonOk = new Button())
+            using (var buttonCancel = new Button())
+            {
+                form.Text = title;
+                label.Text = prompt;
+                textBox.Text = value;
+
+                form.Font = new Font("Tahoma", 8);
+
+                buttonOk.Text = "OK";
+                buttonCancel.Text = "Cancel";
+                buttonOk.DialogResult = DialogResult.OK;
+                buttonCancel.DialogResult = DialogResult.Cancel;
+
+                label.SetBounds(9, 20, 372, 13);
+                textBox.SetBounds(12, 36, 372, 20);
+                buttonOk.SetBounds(228, 72, 75, 23);
+                buttonCancel.SetBounds(309, 72, 75, 23);
+
+                label.AutoSize = true;
+                textBox.Anchor = textBox.Anchor | AnchorStyles.Right;
+                buttonOk.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+                buttonCancel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+
+                form.ClientSize = new Size(396, 107);
+                form.Controls.AddRange(new Control[] { textBox, label, buttonOk, buttonCancel });
+                form.ClientSize = new Size(Math.Max(300, label.Right + 10), form.ClientSize.Height);
+                form.FormBorderStyle = FormBorderStyle.FixedDialog;
+                form.StartPosition = FormStartPosition.CenterScreen;
+                form.MinimizeBox = false;
+                form.MaximizeBox = false;
+                form.AcceptButton = buttonOk;
+                form.CancelButton = buttonCancel;
+
+                DialogResult result = form.ShowDialog();
+                value = textBox.Text;
+                return result;
+            }
+        }
+
         public static void ShowDialog(params UIDocument[] docs)
         {
             Application.EnableVisualStyles();
